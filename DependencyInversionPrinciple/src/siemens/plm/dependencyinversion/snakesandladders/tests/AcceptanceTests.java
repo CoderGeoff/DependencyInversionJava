0cc26161eac70ce17144340b28c1c1a99685e8be
Geoff Bones <geoff.bones@siemens.com>
2015-08-25 12:31:18 +0100

    Add placeholder for acceptance tests

---
+++ DependencyInversionPrinciple/src/siemens/plm/dependencyinversion/snakesandladders/tests/AcceptanceTests.java
@@ -0,0 +1,14 @@
+package siemens.plm.dependencyinversion.snakesandladders.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AcceptanceTests {
+
+	@Test
+	public void givenAPlayerOnSquare1_WhenTheyTakeATurn_TheyShouldMoveForwardTheNumberShownOnTheDie() {
+		fail("Not yet implemented");
+	}
+
+}
