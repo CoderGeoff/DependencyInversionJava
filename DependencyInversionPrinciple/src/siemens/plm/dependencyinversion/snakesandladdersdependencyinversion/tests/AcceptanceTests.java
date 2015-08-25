86a2cf836a44b8c597b555e63e43b3bfde66462c
Geoff Bones <geoff.bones@siemens.com>
2015-08-25 14:23:43 +0100

    Add placeholders for tests

---
+++ DependencyInversionPrinciple/src/siemens/plm/dependencyinversion/snakesandladdersdependencyinversion/tests/AcceptanceTests.java
@@ -0,0 +1,14 @@
+package siemens.plm.dependencyinversion.snakesandladdersdependencyinversion.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AcceptanceTests {
+
+	@Test
+	public void givenBoardWith10Squares_WhenIThrowA10_TheGameShouldEndWithASingleThrow() {
+
+	}
+
+}
---
+++ DependencyInversionPrinciple/src/siemens/plm/dependencyinversion/snakesandladdersdependencyinversion/tests/MoveTests.java
@@ -0,0 +1,17 @@
+package siemens.plm.dependencyinversion.snakesandladdersdependencyinversion.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import siemens.plm.dependencyinversion.snakesandladdersdependencyinversion.IBoard;
+import siemens.plm.dependencyinversion.snakesandladdersdependencyinversion.Move;
+import siemens.plm.dependencyinversion.snakesandladdersdependencyinversion.MoveOutcome;
+
+public class MoveTests {
+
+	@Test
+	public void givenIAmOnSquare2_WhenIMove1Square_IShouldBeOnSquare3() {
+
+	}
+}
